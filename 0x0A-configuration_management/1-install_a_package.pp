c4bcced08ee77f144cf3f23c59a903bec93b5225
Gilt457 <lepatioborel@gmail.com>
2024-03-22 07:51:39 -0700

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,6 @@
 #!/usr/bin/pup
-class flask_install {
-  package { 'python3-pip':
-    ensure => installed,
-  }
-
-  exec { 'install_flask':
-    command => '/usr/bin/pip3 install flask==2.1.0',
-    require => Package['python3-pip'],
-  }
+# Install an especific version of flask (2.1.0)
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
-
-include flask_install
