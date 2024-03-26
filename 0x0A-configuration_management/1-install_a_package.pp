70d436339c52d03fd8829adedaf9215fadd4fa2b
Gilt457 <lepatioborel@gmail.com>
2024-03-26 06:04:16 -0700

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
-# Using Puppet, install flask from pip3
+# Install flask package
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip',
+}
+
+# Install werkzeug package
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip',
 }
