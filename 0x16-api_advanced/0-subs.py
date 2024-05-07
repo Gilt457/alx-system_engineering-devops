d8ee75b29f46f009316684a1d7d2b5e135462ea1
Gilt457 <lepatioborel@gmail.com>
2024-05-07 02:17:56 -0700

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-number of subreddit subscribers
+number of subscribers for a given subreddit
 """
 
 from requests import get
@@ -8,8 +8,8 @@ from requests import get
 
 def number_of_subscribers(subreddit):
     """
-    Function that asks Reddit API to retrieve total
-    subscribers (not active users) for a specific subreddit.
+    function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
     """
 
     if subreddit is None or not isinstance(subreddit, str):
